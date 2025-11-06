883602e2bc7734ba8444eb73079d91548b5465e8
yuming <mingjiangyu1@qq.com>
2025-11-06 17:49:46 +0800

    feat: 添加 banner 图片和个人相关信息
    - 添加新的 icons package 以支持微信图标
    - 添加头像和主页横幅图片
    - 在 `src/config.ts` 中添加个人信息和自我介绍
    - 重写 `src/spec/` 下的 about me 文件
    - 调整 `pages/about.astro` 的选择展示逻辑，使其根据网站的语言码动态决定展示中文/英文的about

--- src/config.ts
+++ src/config.ts
@@ -8,21 +8,21 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "MiWu",
+	subtitle: "myblog", //TODO: 起一个好用的子标题
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "Artwork by @Neneko_sleep", // Credit text to be displayed
+			url: "https://x.com/Neneko_sleep/status/1743969474330644607/photo/1", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -46,33 +46,38 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/mistcoversmyeyes", // 指向你的 GitHub 主页
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avater.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "MiWu",
+	bio: "An undergraduate from SCUT，majoring in CS.Hope you can get some idea from my posts!",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "GitHub",
+			icon: "fa6-brands:github", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://github.com/mistcoversmyeyes",
 		},
 		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			name: "Email",
+			icon: "mdi:email",
+			url: "mingjiangyu1@qq.com",
 		},
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			name: "知乎",
+			icon: "fa6-brands:zhihu",
+			url: "https://www.zhihu.com/people/your-username", // 请替换为你的知乎主页
+		},
+		{
+			name: "Wechat",
+			icon: "tabler:brand-wechat",
+			url: "https://weixin.qq.com", // 请替换为微信公众号主页
 		},
 	],
 };
